1ed78ceb72fb07641fac193b592a117580a91ca5
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-01-02 21:43:37 -0500

    Added custom scripting for the 'healing potion' object and added logic indexing inventory items

--- Heros_Inventory_2.py
+++ Heros_Inventory_2.py
@@ -11,3 +11,15 @@ for item in inventory:
     print(item)
     
 input("\nPress the enter key to continue.")
+
+# get the length of a tuple
+print("You have", len(inventory), "items in your possession.")
+
+# test for membership with in
+if "healing potion" in inventory:
+    print("You will live to fight another day.")
+
+index = int(input("\nEnter the index number for an item in inventory: "))
+print("At index", index, "is", inventory[index])
+
+input("\nPress the enter key to continue.")
