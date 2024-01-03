10949f49f1f700b2884453f5f169015ae71c6c6c
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-01-02 20:20:12 -0500

    Created an empty tuple, and made it a condition.

--- Heros_Inventory.py
+++ Heros_Inventory.py
@@ -1,2 +1,11 @@
 #Hero's Inventory
 #Demonstrates tuple creation
+
+#create an empty tuple
+inventory = ()
+
+#treat the tuple as a condition
+if not inventory:
+    print("You are empty-handed.")
+    
+input("\nPress the enter key to continue.")
