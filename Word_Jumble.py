1a5b5ca3f8a5da8bcba3120ce45e99ccdd0c5ea2
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-01-03 21:59:25 -0500

    Commiting before working through the jumble mechanic.

--- Word_Jumble.py
+++ Word_Jumble.py
@@ -1,1 +1,15 @@
 # Word Jumble
+#
+# The computer picks a random word and then "jumbles" it
+# The player has to guess the original word
+
+import random
+
+# create a sequence of words to choose from
+WORDS = ("python", "jumble", "easy", "difficult", "answer", "xylophone")
+
+# pick one word randomly from the sequence
+word = random.choice(WORDS)
+
+# create a variable to use later to see if the guess is correct
+correct = word
