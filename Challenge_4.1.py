9477f349610806a7d23b6edcbb6f8b445d35f524
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-01-06 16:10:00 -0500

    Created the initial shell of the program and added the Title

---
+++ Challenge_4.1.py
@@ -0,0 +1,12 @@
+# Challenge 4.1
+# Write a program that counts for the user.
+# Let the user enter the starting number, the ending number, and the anount by which to count.
+
+print(
+"""
+                    Welcome to the Counter Program
+                This program will count from a starting nummber of your choosing
+                to the end number of your choosing, and count by the denomination
+                that you choose.
+    """
+)
